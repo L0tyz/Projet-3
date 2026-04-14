8ad34d7dcd8102869fd46a758ef3cee87f8e1d3c
cmartin-3360 <casseymartinff@gmail.com>
2026-04-14 16:25:19 -0400

    Ajout de constantes

---
+++ snake-ladders/minigames/hangman/hang_constantes.py
@@ -0,0 +1,26 @@
+class hang_constantes:
+    ### Ecran ###
+    entete = str("Hangman")
+    largeur_ecran = int(600)
+    hauteur_ecran = int(600)
+
+    ### Couleurs ###
+    rgb_blanc = (255, 255, 255) 
+    rgb_bleu = (0, 200, 255)
+    rgba_bleu = (0, 200, 255, 22)
+    rgb_rouge = (255, 0, 0)
+    rgba_rouge = (255, 0, 0, 22)
+    rgb_noir = (0, 0, 0) 
+
+    ### Nom fichier images ###
+    barbie_hache = "Assets/BarbieHache.png"
+    barbie_marteau = "Assets/BarbieMarteau.png"
+    barbie_tt_seule = "Assets/BarbieTTSeule.png"
+    hanche = "Assets/hanche.png"
+    hammer = "Assets/hammer.png"
+    ken_bras_droit = "Assets/Ken_Bras_Droit.png"
+    ken_bras_gauche = "Assets/Ken_Bras_Gauche.png"
+    ken_jambe_droite = "Assets/Ken_Jambe_Droite.png"
+    ken_jambe_gauche = "Assets/Ken_Jambe_Gauche.png"
+    ken_tete = "Assets/Ken_Tête.png"
+    ken_torse = "Assets/Ken_Torse.png"
