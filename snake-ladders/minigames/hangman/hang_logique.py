5edcbb02864b59afac321bff377b0de4ab742f95
cmartin-3360 <casseymartinff@gmail.com>
2026-04-14 16:11:11 -0400

    Ajout decran pour hangman

--- snake-ladders/minigames/hangman/hang_logique.py
+++ snake-ladders/minigames/hangman/hang_logique.py
@@ -2,3 +2,12 @@
     Fichier contenant la machine à état contrôlant le jeu hangman logiquement avec les echecs/outils et la banque de mots
     Realiser par Cassey Martin et Jake Chagnon
 """
+
+# random.randint(0, W - 20) import random
+
+
+class hang_logique:
+
+    @staticmethod
+    def millisToSeconds(millis):
+        return float(millis / 1000)
