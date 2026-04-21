ccb656f653d6518dfaf427bd545b074aa92eca54
Vinithepoo0901 <vincentgoulet0901@gmail.com>
2026-04-21 14:05:06 -0400

    y

--- snake_ladders/minigames/tetris/blocks.py
+++ snake_ladders/minigames/tetris/blocks.py
@@ -2,6 +2,9 @@ from block import Block
 from position import Position
 
 class LBlock(Block):
+    """
+    Classe définissant le block L et lui donnant une valeur (id)
+    """
     def __init__(self):
         super().__init__(id = 1)
         self.cells = {
@@ -10,3 +13,79 @@ class LBlock(Block):
             2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
             3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
         }
+
+class JBlock(Block):
+    """
+    Classe définissant le block J et lui donnant une valeur (id)
+    """
+    def __init__(self):
+        super().__init__(id = 2)
+        self.cells = {
+            0: [Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)]
+        }
+
+class IBlock(Block):
+    """
+    Classe définissant le block I et lui donnant une valeur (id)
+    """
+    def __init__(self):
+        super().__init__(id = 3)
+        self.cells = {
+            0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
+            1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
+            2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)]
+        }
+
+class OBlock(Block):
+    """
+    Classe définissant le block O (carre) et lui donnant une valeur (id)
+    """
+    def __init__(self):
+        super().__init__(id = 4)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
+        }
+
+class SBlock(Block):
+    """
+    Classe définissant le block S et lui donnant une valeur (id)
+    """
+    def __init__(self):
+        super().__init__(id = 5)
+        self.cells = {
+            0: [Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)],
+            2: [Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)],
+            3: [Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)]
+        }
+
+class TBlock(Block):
+    """
+    Classe définissant le block T et lui donnant une valeur (id)
+    """
+    def __init__(self):
+        super().__init__(id = 6)
+        self.cells = {
+            0: [Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 1)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)]
+        }
+
+class ZBlock(Block):
+    """
+    Classe définissant le block Z et lui donnant une valeur (id)
+    """
+    def __init__(self):
+        super().__init__(id = 7)
+        self.cells = {
+            0: [Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)],
+            2: [Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2)],
+            3: [Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)]
+        }
+
