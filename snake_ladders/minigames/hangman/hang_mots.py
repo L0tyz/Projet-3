c05ac1c07c1e67ab91f8f2b4ca0d7a0b6574cb53
Zeruel-Ara <jacobchagnon506@gmail.com>
2026-04-21 14:31:45 -0400

    Update hang_mots.py

--- snake_ladders/minigames/hangman/hang_mots.py
+++ snake_ladders/minigames/hangman/hang_mots.py
@@ -1,16 +1,7 @@
 """
 Banque de mots pour le jeu hangman
 Créé par Cassey Martin et Jake Chagnon
--Condensateur
--Régulateur
--Multimètre
--Oscilloscope
--Impédance
--Amplificateur
--Microcontroleur
--Tension
--
 """
 import random
 banque_mots = ["Condensateur", "Regulateur", "Multimetre", "Oscilloscope", "Impédance", "Amplificateur", "Microcontroleur", "Tension"]
-mot_choisi = random.choice(banque_mots) #choisit un mot au hasard parmi la liste
+mot_choisi = random.choice(banque_mots) #choisit un mot au hasard parmis la liste
