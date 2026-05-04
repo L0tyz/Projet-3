417344eb02af16c74d2ef1e81b7986c7df828ae5
tommybrunelle29 <tommy.brunelle29@gmail.com>
2026-05-03 21:56:30 -0400

    pong maintenant tout en classe

--- snake_ladders/minigames/pong/pong.py
+++ snake_ladders/minigames/pong/pong.py
@@ -1,128 +1,77 @@
 import pygame
-import random
-import mouvement
-import rebond
+from classe_joueur import joueur
+from classe_adversaire import adversaire
+from classe_balle import balle
 
 pygame.init()
 
-# Taille de l'écran de jeu.
 ecran = pygame.display.set_mode((720,720))
 clock = pygame.time.Clock()
 
 running = True
+
+# Delta time. Permet plus tard d'ajuster le FPS.
 dt = 0
 
+# Scores initiaux.
 score_joueur = 0
-score_adversaire = 0 
-
-police = pygame.font.SysFont("consolas", 80, bold=True)
+score_adversaire = 0
 
 victoire = 5
 
-# Vitesse joueur.
-vitesse = 200
-vitesse_adversaire = 350
+# Police pour le score.
+police = pygame.font.SysFont("consolas", 80, bold=True)
 
-# Informations de la balle.
-coordonnee_balle_x = 360 # Position de départ sur l'axe X
-coordonnee_balle_y = 360 # Position de départ sur l'axe Y
-rayon = 15
-
-# Trajectoire initiale de balle en diagonale.
-trajectoire_x = 250
-trajectoire_y = 180
-
-# Vitesse initaile de la balle.
-vitesse_balle = 300
-
-# Créer le rectangle joueur. Sa position et sa taille.
-joueur = pygame.Rect(680, 300, 20, 80)
-
-# Créer le rectangle adversaire.
-adversaire = pygame.Rect(20, 300, 20, 80)
+# Les objets.
+joueur = joueur()
+adversaire = adversaire()
+balle = balle()
 
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
 
-    # Couleur de background.
     ecran.fill("black")
 
-    # Restreindre la position des joueurs dans la taille de l'écran.
-    joueur.clamp_ip(ecran.get_rect())
-    adversaire.clamp_ip(ecran.get_rect())
+    joueur.mouvement(dt)
+    adversaire.mouvement(dt, balle.position_y)
+    balle.mouvement(dt)
 
-    # Mouvement joueur.
-    mouvement.joueur(joueur, dt)
+    balle.rebonds(joueur, adversaire)
 
-    # Mouvement adversaire.
-    mouvement.adversaire(adversaire, dt, coordonnee_balle_y, vitesse_adversaire)
-    
-    # Mouvement de la balle (en diagonale).
-    coordonnee_balle_x, coordonnee_balle_y = mouvement.balle(
-        coordonnee_balle_x, 
-        coordonnee_balle_y, 
-        trajectoire_x, 
-        trajectoire_y, 
-        dt
-    )
-    # Rebonds de la balle.
-    coordonnee_balle_x, coordonnee_balle_y, trajectoire_x, trajectoire_y, vitesse_balle = rebond.rebonds(
-        coordonnee_balle_y, 
-        coordonnee_balle_x,
-        rayon,
-        trajectoire_y,
-        trajectoire_x,
-        joueur,
-        adversaire,
-        vitesse_balle
-        )
+    # Limiter le déplacement des joueurs dans l'écran.
+    joueur.rect.clamp_ip(ecran.get_rect())
+    adversaire.rect.clamp_ip(ecran.get_rect())
 
-    # Si la balle sort à droite ou à gauche, on réinitialise la position de la balle et sa trajectoire.
-    if coordonnee_balle_x - rayon > ecran.get_width():
-        # Perdu.
-        coordonnee_balle_x = 360
-        coordonnee_balle_y = 360
-        trajectoire_x = random.choice([-250, 250])
-        trajectoire_y = random.randint(-200,200)
-        vitesse_balle = 300
-        joueur = pygame.Rect(680, 300, 20, 80)
-        adversaire = pygame.Rect(20, 300, 20, 80)
+    # point adversaire
+    if balle.position_x - balle.rayon > 720:
         score_adversaire += 1
+        balle.reinitialiser()
+        joueur.reinitialiser()
+        adversaire.reinitialiser()
 
-        if score_adversaire == victoire:
-            running = False
-
-    if coordonnee_balle_x + rayon < 0:
-        # gagné.
-        coordonnee_balle_x = 360
-        coordonnee_balle_y = 360
-        trajectoire_x = random.choice([-250, 250])
-        trajectoire_y = random.randint(-200,200)
-        vitesse_balle = 300
-        joueur = pygame.Rect(680, 300, 20, 80)
-        adversaire = pygame.Rect(20, 300, 20, 80)
+    # point joueur
+    if balle.position_x + balle.rayon < 0:
         score_joueur += 1
+        balle.reinitialiser()
+        joueur.reinitialiser()
+        adversaire.reinitialiser()
 
-        if score_joueur == victoire:
-            running = False
+    # victoire
+    if score_joueur == victoire or score_adversaire == victoire:
+        running = False
 
-    # Dessiner le rectangle (joueur).
-    pygame.draw.rect(ecran, ("white"), joueur)
+    # dessins
+    joueur.dessiner(ecran)
+    adversaire.dessiner(ecran)
+    balle.dessiner(ecran)
 
-    # Dessiner le rectangle (adversaire).
-    pygame.draw.rect(ecran, ("white"), adversaire)
-
-    # Dessiner la balle.
-    pygame.draw.circle(ecran, ("white"), (coordonnee_balle_x, coordonnee_balle_y), rayon)
-
-    texte_score = police.render(f"{score_adversaire} - {score_joueur}", False, ("white"))
-    centre_score = texte_score.get_rect(center=(360, 50))
-    ecran.blit(texte_score, centre_score)
+    texte = police.render(f"{score_adversaire} - {score_joueur}", False, "white")
+    ecran.blit(texte, texte.get_rect(center=(360,50)))
 
     pygame.display.flip()
-    dt = clock.tick(60) / 1000 #16 msec entre chaque frame
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
